21ad270562de90458196652484d848640737b07e
Crims0nCrusader <hjbpucci@gmail.com>
2024-01-18 13:29:48 -0800

    first commit

--- core/src/com/bhms/gdxintro/AppHandler.java
+++ core/src/com/bhms/gdxintro/AppHandler.java
@@ -5,18 +5,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.bhms.gdxintro.gfx.utils.TileHandler;
 
 public class AppHandler extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 
 	int x, y;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 
 		x = 0;
 		y = 0;
@@ -27,13 +28,27 @@ public class AppHandler extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 
-		batch.draw(img, x, y);
+		for(int r = 0; r < Gdx.graphics.getHeight(); r+=64) {
+			for(int c = 0; c < Gdx.graphics.getWidth(); c+=64) {
+				batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(x++).getTexture(), c, r);
+			if(x > 4) {
+				x = 0;
+			}
+			}
+
+		}
+
+
+		batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(1).getTexture(), 128, 64);
+		batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(2).getTexture(), 192, 64);
+		batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(3).getTexture(), 450, 64);
+		batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(4).getTexture(), 350, 64);
+		batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(5).getTexture(), 500, 64);
 
 		batch.end();
 
 		checkInput();
 	}
-
 	public void checkInput() {
 		/*
 		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
@@ -53,27 +68,27 @@ public class AppHandler extends ApplicationAdapter {
 
 		x = Gdx.input.getX() - img.getWidth()/2;
 		y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
-		*/
+
 
 		if(x < (Gdx.input.getX() - img.getWidth()/2)) {
-			x+=5;
+			x+=10;
 		}
 		if(x > (Gdx.input.getX() - img.getWidth()/2)) {
-			x-=5;
+			x-=10;
 		}
 		if(y > (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
-			y-=5;
+			y-=10;
 		}
 		if(y < (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
-			y+=5;
+			y+=10;
 		}
-
+		*/
 
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
---
+++ core/src/com/bhms/gdxintro/gfx/utils/TileHandler.java
@@ -0,0 +1,62 @@
+package com.bhms.gdxintro.gfx.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+import com.bhms.gdxintro.gfx.assets.Tile;
+import com.bhms.gdxintro.gfx.assets.WorldTile;
+
+public class TileHandler {
+
+    public static TileHandler tileHandler = null;
+
+    private int spriteOnSheetHeight, spriteOnSheetWidth;
+
+    private String primaryColorSheetPath = "primaryColorSheet.png";
+
+    private Array<WorldTile> worldTileArray;
+
+    private TileHandler() {
+        worldTileArray = new Array<>();
+
+        spriteOnSheetHeight = Tile.ON_SCREEN_DEFAULT_HEIGHT;
+        spriteOnSheetWidth = Tile.ON_SCREEN_DEFAULT_WIDTH;
+
+        Texture tempTexture = new Texture(primaryColorSheetPath);
+        TextureRegion tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*2, spriteOnSheetHeight*0, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile blackWorldTile = new WorldTile(tempRegion, 0, "black");
+        worldTileArray.add(blackWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*1, spriteOnSheetHeight*0, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile whiteWorldTile = new WorldTile(tempRegion, 1, "white");
+        worldTileArray.add(whiteWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*2, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile redWorldTile = new WorldTile(tempRegion, 2, "red");
+        worldTileArray.add(redWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*7, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile blueWorldTile = new WorldTile(tempRegion, 3, "blue");
+        worldTileArray.add(blueWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*6, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile greenWorldTile = new WorldTile(tempRegion, 4, "green");
+        worldTileArray.add(greenWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*5, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile limeWorldTile = new WorldTile(tempRegion, 5, "lime");
+        worldTileArray.add(limeWorldTile);
+
+    }
+
+    public Array<WorldTile> getWorldTileArray() {
+        return worldTileArray;
+    }
+
+    public static TileHandler getTileHandler() {
+        if(tileHandler == null) {
+            tileHandler = new TileHandler();
+        }
+        return tileHandler;
+    }
+}
